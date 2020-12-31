9cdcc32485d2fca92a7c7a5b40a54de97865a89f
PJW527 <wlsdnjs527@naver.com>
2020-12-31 11:27:52 +0900

    git test.java 수정

--- src/stsGitTest/Gittest.java
+++ src/stsGitTest/Gittest.java
@@ -4,7 +4,8 @@ public class Gittest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		// 폴더를 눌러서 history 확인이 가능하다. git을 쓰는 소스트리에서 봤을듯
+		System.out.println("sts에서 git 사용 테스트");
 	}
 
 }
