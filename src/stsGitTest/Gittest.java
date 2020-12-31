be0cd9320ec798978989a892921b0ffdb381ccb6
PJW527 <wlsdnjs527@naver.com>
2020-12-31 11:46:04 +0900

    sts에서 수정후 소스트리에서 커밋

--- src/stsGitTest/Gittest.java
+++ src/stsGitTest/Gittest.java
@@ -6,6 +6,7 @@ public class Gittest {
 		// TODO Auto-generated method stub
 		// 폴더를 눌러서 history 확인이 가능하다. git을 쓰는 소스트리에서 봤을듯
 		System.out.println("sts에서 git 사용 테스트");
+		System.out.println("sts와 소스트리 연동 테스트1");
 	}
 
 }
